dac9654a58ab47664fa3534c61d4097f72083456
Esse <w.vanesse@hhnk.nl>
2023-07-07 15:51:23 +0200

    werkende versie juli 2023

--- datachecker/scripts/polder/01_lizard_db_vullen/02_load_data.cmd
+++ datachecker/scripts/polder/01_lizard_db_vullen/02_load_data.cmd
@@ -1,8 +1,8 @@
-echo "Loading datamining AHN3 data @ /code/data/fixed/datamining_ahn3/datamining_ahn3.shp"
-ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=datachecker password=postgres port=5432" -lco "GEOMETRY_NAME=wkb_geometry" -lco "SCHEMA=fixed_data" -lco "FID=objectid" -nlt MULTIPOLYGON -lco "OVERWRITE=YES" -progress /code/data/fixed_data/datamining_ahn3/datamining_ahn3.shp -skipfailures
-echo "Loading connection CODE/HydrObject @ /code/data/fixed/hydroobjectid/CODE_HydroObject_OBJECTID.dbf"
-ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=datachecker password=postgres port=5432" -lco "GEOMETRY_NAME=wkb_geometry" -lco "SCHEMA=fixed_data" -lco "OVERWRITE=YES" -progress /code/data/fixed_data/hydroobjectid/CODE_HydroObject_OBJECTID.dbf -skipfailures
-echo "Loading DAMO data @ /code/data/input/DAMO.gdb"
-ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=datachecker password=postgres port=5432" -nlt CONVERT_TO_LINEAR -lco "GEOMETRY_NAME=wkb_geometry" -lco "SCHEMA=damo_ruw" -lco "FID=objectid" -lco "OVERWRITE=YES" -progress /code/data/input/DAMO.gdb --config PG_USE_COPY YES -gt 1000
-echo "Loading HDB data @ /code/data/input/HDB.gdb"
-ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=datachecker password=postgres port=5432" -nlt CONVERT_TO_LINEAR -lco "GEOMETRY_NAME=wkb_geometry" -lco "SCHEMA=hdb" -lco "FID=objectid" -lco "OVERWRITE=YES" -progress /code/data/input/HDB.gdb -skipfailures
+echo "Loading datamining AHN3 data @ \\corp.hhnk.nl\data\Hydrologen_data\Data\modelbuilder\data\fixed\datamining_ahn3\datamining_ahn3.shp"
+ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=datachecker password=postgres port=5433" -lco "GEOMETRY_NAME=wkb_geometry" -lco "SCHEMA=fixed_data" -lco "FID=objectid" -nlt MULTIPOLYGON -lco "OVERWRITE=YES" -progress \\corp.hhnk.nl\data\Hydrologen_data\Data\modelbuilder\data\fixed_data\datamining_ahn3\datamining_ahn3.shp -skipfailures
+echo "Loading connection CODE/HydrObject @ \\corp.hhnk.nl\data\Hydrologen_data\Data\modelbuilder\data\fixed\hydroobjectid\CODE_HydroObject_OBJECTID.dbf"
+ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=datachecker password=postgres port=5433" -lco "GEOMETRY_NAME=wkb_geometry" -lco "SCHEMA=fixed_data" -lco "OVERWRITE=YES" -progress \\corp.hhnk.nl\data\Hydrologen_data\Data\modelbuilder\data\fixed_data\hydroobjectid\CODE_HydroObject_OBJECTID.dbf -skipfailures
+echo "Loading DAMO data @ \\corp.hhnk.nl\data\Hydrologen_data\Data\modelbuilder\data\input\DAMO.gdb"
+ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=datachecker password=postgres port=5433" -nlt CONVERT_TO_LINEAR -lco "GEOMETRY_NAME=wkb_geometry" -lco "SCHEMA=damo_ruw" -lco "FID=objectid" -lco "OVERWRITE=YES" -progress \\corp.hhnk.nl\data\Hydrologen_data\Data\modelbuilder\data\input\DAMO.gdb --config PG_USE_COPY YES -gt 1000
+echo "Loading HDB data @ \\corp.hhnk.nl\data\Hydrologen_data\Data\modelbuilder\data\input\HDB.gdb"
+ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=datachecker password=postgres port=5433" -nlt CONVERT_TO_LINEAR -lco "GEOMETRY_NAME=wkb_geometry" -lco "SCHEMA=hdb" -lco "FID=objectid" -lco "OVERWRITE=YES" -progress \\corp.hhnk.nl\data\Hydrologen_data\Data\modelbuilder\data\input\HDB.gdb -skipfailures
--- modelbuilder/scripts/polder/25_export_model_to_sqlite.cmd
+++ modelbuilder/scripts/polder/25_export_model_to_sqlite.cmd
@@ -1,7 +1,7 @@
-mkdir .\code\tmp\models -p
-python .\code\modelbuilder\tools\threedi-export\export_threedi.py \code\tmp\models\bwn_$2.sqlite
-python .\code\modelbuilder\tools\modelchecker.py \code\tmp\models\bwn_$2.sqlite
-rmdir /s /q .\code\data\output\models\*
-mkdir .\code\data\output\models -p
-xcopy /E /I /Y .\code\tmp\models .\code\data\output\models
-rmdir /s /q .\code\tmp
+mkdir \\corp.hhnk.nl\data\Hydrologen_data\Data\modelbuilder\data\tmp\models
+python .\code\modelbuilder\tools\threedi-export\export_threedi.py \\corp.hhnk.nl\data\Hydrologen_data\Data\modelbuilder\data\tmp\models\bwn_%2.sqlite
+rem python .\code\modelbuilder\tools\modelchecker.py .\code\tmp\models\bwn_%2.sqlite
+rmdir /s /q \\corp.hhnk.nl\data\Hydrologen_data\Data\modelbuilder\data\output\models
+mkdir \\corp.hhnk.nl\data\Hydrologen_data\Data\modelbuilder\data\output\models
+xcopy /E /I /Y \\corp.hhnk.nl\data\Hydrologen_data\Data\modelbuilder\data\tmp\models \\corp.hhnk.nl\data\Hydrologen_data\Data\modelbuilder\data\output\models
+rem rmdir /s /q \\corp.hhnk.nl\data\Hydrologen_data\Data\modelbuilder\data\tmp
